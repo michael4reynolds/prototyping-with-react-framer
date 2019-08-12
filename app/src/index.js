4eb93b958a6931f2e3180bb771106940a4af7693
Michael Reynolds <michael4reynolds@gmail.com>
2019-08-12 16:14:04 -0400

    first commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,40 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Frame } from "framer"
+import { Frame, useCycle } from "framer"
 import "./styles.css"
 
+// Parameter
+function microwave(food) {
+  // function body
+  // return value
+  return "heated " + food
+}
+
+// defining a function
+function sandwichMaker(meat) {
+  let sandwich = "🍞"
+  sandwich = sandwich + meat
+  sandwich = sandwich + "🍀"
+  sandwich = sandwich + "🍅"
+  sandwich = sandwich + "🍞"
+  return sandwich
+}
+
 function App() {
+  const [knobX, cycleKnobX] = useCycle(0, 60)
   return (
     <div className="App">
-      <Frame width={120} height={60} borderRadius={30}>
-        <Frame size={60} borderRadius={30} />
+      <div>{sandwichMaker("🥓")}</div>
+      <Frame
+        width={120}
+        height={60}
+        borderRadius={30}
+        onTap={function handleTap() {
+          console.log(knobX)
+          cycleKnobX()
+        }}
+      >
+        <Frame size={60} borderRadius={30} x={knobX} />
       </Frame>
     </div>
   )
